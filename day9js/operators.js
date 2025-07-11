ba77e8aca5f4798066729b05a83e03ffa0eaa102
Jaswanthnayak45 <jaswanth.nayak2005@gmail.com>
2025-07-11 17:38:40 +0530

    Create operators.js

---
+++ day9js/operators.js
@@ -0,0 +1,33 @@
+//logical operator
+let a=true;
+let b=false;
+console.log(a&&b);
+console.log(a||b);
+console.log(!a) 
+//variables
+let name="jaswanth";
+var age1=19;
+const PI=3.14;
+console.log(name);
+console.log(age1)
+console.log(PI)
+//types of variable
+console.log(typeof(name));
+console.log(typeof(PI))
+//comparison operator
+console.log(5=="5");
+console.log(5==="5");
+console.log(10>5)
+//arithetic operator
+let result=10+5;
+let result2=10-5;
+let result3=10*5;
+let result4=10%5;
+console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+//ternary operator
+let Age=18;
+let status=(Age>=18)? "Adult" : "Minor"
+console.log(status);
